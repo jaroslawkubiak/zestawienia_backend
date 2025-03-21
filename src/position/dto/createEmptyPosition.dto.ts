5d1e2ced048668a6d08332df2efccef44312f6b3
jaroslawkubiak <jaroslawkubiak82@gmail.com>
2025-03-21 22:42:30 +0100

    remove supplier property

--- src/position/dto/createEmptyPosition.dto.ts
+++ src/position/dto/createEmptyPosition.dto.ts
@@ -2,7 +2,6 @@ import { Type } from 'class-transformer';
 import { IsNumber, ValidateNested } from 'class-validator';
 import { BookmarkDto } from '../../bookmarks/dto/bookmark.dto';
 import { NewSetForPositionDto } from '../../sets/dto/NewSet.dto';
-import { UpdateSupplierDto } from '../../suppliers/dto/supplier.dto';
 import { CreateUserDto } from '../../user/dto/user.dto';
 
 export class CreateEmptyPositionDto {
@@ -12,10 +11,6 @@ export class CreateEmptyPositionDto {
   @ValidateNested({ each: true })
   @Type(() => BookmarkDto)
   bookmarkId: BookmarkDto;
-
-  @ValidateNested({ each: true })
-  @Type(() => UpdateSupplierDto)
-  supplierId: UpdateSupplierDto;
 
   @ValidateNested({ each: true })
   @Type(() => CreateUserDto)

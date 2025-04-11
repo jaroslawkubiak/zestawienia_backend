37d43f9ca11a8d721528d6ca502f0cfb2ae2b3e2
jaroslawkubiak <jaroslawkubiak82@gmail.com>
2025-04-11 18:08:46 +0200

    refactor emal dto - clientId or supplierId has to be present

--- src/email/dto/email.dto.ts
+++ src/email/dto/email.dto.ts
@@ -1,4 +1,27 @@
-import { IsNumber, IsOptional, IsString } from 'class-validator';
+import {
+  IsString,
+  IsNumber,
+  IsOptional,
+  Validate,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  ValidationArguments,
+} from 'class-validator';
+
+// Custom validator : one of clientId or supplierId has to be present
+@ValidatorConstraint({ name: 'ClientOrSupplier', async: false })
+export class ClientOrSupplierConstraint
+  implements ValidatorConstraintInterface
+{
+  validate(_: any, args: ValidationArguments) {
+    const obj = args.object as any;
+    return !!(obj.clientId || obj.supplierId);
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    return 'Either clientId or supplierId must be provided.';
+  }
+}
 
 export class SendEmailDto {
   @IsString()
@@ -26,4 +49,7 @@ export class SendEmailDto {
 
   @IsString()
   link: string;
+
+  @Validate(ClientOrSupplierConstraint)
+  validateClientOrSupplier: boolean;
 }

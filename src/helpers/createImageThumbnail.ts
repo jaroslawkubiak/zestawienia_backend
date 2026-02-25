7ac1dc01308602fa85d8de4fdcdf9fae02d9bf5e
jaroslawkubiak <jaroslawkubiak82@gmail.com>
2026-02-25 20:10:58 +0100

    add try catch to method

--- src/helpers/createImageThumbnail.ts
+++ src/helpers/createImageThumbnail.ts
@@ -12,47 +12,56 @@ export async function createImageThumbnail(
   const MAX_DIMENSION = 1000;
   const MINI_DIMENSION = 400;
 
-  const input = file.buffer ?? file.path;
-
-  if (!input) {
-    throw new Error('Brak źródła obrazu (buffer/path)');
-  }
-
-  const metadata = await sharp(input).metadata();
-
   const processFile: IProcessFile = {
     dimensions: {
-      width: metadata.width ?? 0,
-      height: metadata.height ?? 0,
+      width: 0,
+      height: 0,
     },
+    thumbnailFileName: '',
+    thumbnailPath: '',
   };
 
-  // if image is small - don't generate thumbnail
-  if (
-    (metadata.width ?? 0) < MAX_DIMENSION &&
-    (metadata.height ?? 0) < MAX_DIMENSION
-  ) {
+  try {
+    const input = file.buffer ?? file.path;
+
+    if (!input) {
+      throw new Error('Brak źródła obrazu (buffer/path)');
+    }
+
+    const metadata = await sharp(input).metadata();
+
+    processFile.dimensions.width = metadata.width ?? 0;
+    processFile.dimensions.height = metadata.height ?? 0;
+
+    // if image is small - don't generate thumbnail
+    if (
+      (metadata.width ?? 0) < MAX_DIMENSION &&
+      (metadata.height ?? 0) < MAX_DIMENSION
+    ) {
+      return processFile;
+    }
+
+    // generate name
+    const ext = path.extname(originalFileName);
+    const nameWithoutExt = path.basename(originalFileName, ext);
+    const thumbnailFileName = `${nameWithoutExt}_mini${ext}`;
+    const miniPath = path.join(uploadPath, thumbnailFileName);
+
+    // save thumbnail
+    await sharp(input)
+      .resize({
+        width: MINI_DIMENSION,
+        height: MINI_DIMENSION,
+        fit: 'inside',
+        withoutEnlargement: true,
+      })
+      .toFile(miniPath);
+
+    processFile.thumbnailFileName = thumbnailFileName || '';
+    processFile.thumbnailPath = miniPath || '';
+
     return processFile;
+  } catch (error) {
+    throw new Error(error);
   }
-
-  // generate name
-  const ext = path.extname(originalFileName);
-  const nameWithoutExt = path.basename(originalFileName, ext);
-  const thumbnailFileName = `${nameWithoutExt}_mini${ext}`;
-  const miniPath = path.join(uploadPath, thumbnailFileName);
-
-  // save thumbnail
-  await sharp(input)
-    .resize({
-      width: MINI_DIMENSION,
-      height: MINI_DIMENSION,
-      fit: 'inside',
-      withoutEnlargement: true,
-    })
-    .toFile(miniPath);
-
-  processFile.thumbnailFileName = thumbnailFileName;
-  processFile.thumbnailPath = miniPath;
-
-  return processFile;
 }
